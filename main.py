dd40ad870a9889cc019272f6d0832bdac0388603
shadowforce78 <planque.adam@gmail.com>
2024-11-06 16:27:01 +0100

    add password management functionality and create data.json file on first start

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# Hello World :)
+import cryptography
+import os
+
+file = 'data.json'
+
+def first_start(file):
+    if not os.path.exists(file):
+        password = input('Enter password: ')
+        create_file(file, password)
+    else:
+        password = input('Enter password: ')
+    return password
+
+
+def create_file(file, password):
+    # Create data.json and add password as a parent element for other data to be stored
+    with open(file, 'w') as f:
+        f.write('{"password": "' + password + '"}')
+def main():
+    password = first_start(file)
+    print(password)
+    
+main()
